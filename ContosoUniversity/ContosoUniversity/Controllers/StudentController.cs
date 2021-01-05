f939c1d287f61f990f39452638d8069218f1834d
levi <levicaseblodgett@gmail.com>
2021-01-05 13:36:12 -0800

    added CRUD functionality
    -added CRUD functionality for the students by updating several functions and pages (see below)
    -updated "Create" HTTPost method to handle if invalid info is entered when creating a new student
    -updated "Edit" HTTPost method to handle if invalid info is entered when changing the student, which means that changes made to the database are more secure
    -removed the "Bind" attribute from the "Edit" method, so that it now no longer overwrites pre-existing values for students in the database if the new values the user enters are invalid or not defined
    -updated "Delete" HTTpost method and the regular "Delete" method to ask the user if they're sure they want to delete a Student in the database, and to prevent any deletions made that are invalid (such as if they attempt to delete a non-existent student)
    -updated "Details" method to handle if the user attempts to search for the details of an invalid student
    -updated the "Details.cshtml" view to display the Enrollment property of each student object, as well as all the properties within that Enrollment (course title, course ID, how many credits its worth, etc.)

--- ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -24,15 +24,19 @@ namespace ContosoUniversity.Controllers
         // GET: Students/Details/5
         public ActionResult Details(int? id)
         {
+            //if the ID passed into this method is null, displays an error page
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            //attempts to locate the Student object in the database with the matching ID property, and save it to a new Student object
             Student student = db.Students.Find(id);
+            //if it is unable to locate a student with the matching ID, the Student object will be null, and it will display a webpage saying it could not be found
             if (student == null)
             {
                 return HttpNotFound();
             }
+            //in the end, if it is able to locate a student with a matching ID in the database and save it to a Student object, it will display the Details page for that Student object
             return View(student);
         }
 
@@ -47,13 +51,26 @@ namespace ContosoUniversity.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LastName,FirstName,EnrollmentDate")] Student student)
+        public ActionResult Create([Bind(Include = "LastName,FirstName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            //attempts to add the student to the database
+            try
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    //adds the student to the Students table in the database, saves the changes made to the database, then displays the Students/Index page
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            //handles any DataExcepton errors that occur
+            catch (DataException dex)
+            {
+                //logs a standard error message
+                ModelState.AddModelError("", "Unable to save changes. Try again later, and if the problem persists, contact your system administrator.");
+                //logs the error message for the specific error that occurred in this instance
+                ModelState.AddModelError("", dex.Message);
             }
 
             return View(student);
@@ -75,29 +92,63 @@ namespace ContosoUniversity.Controllers
         }
 
         // POST: Students/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        // I updated the original Edit method and changed it to this, to prevent overposting. The reason I removed the "Bind" attribute (the attribute that binds the info passed in
+        // to a Student object) is because the "Bind" attribute actually clears out any pre-existing values in the object it makes. Because it clears that out, invalid info could
+        // potentially replace previously existing valid info for the object it's attempting to edit. So, I removed the "Bind" attribute and instead put in safeguards such as
+        // "TryUpDateModel" and a "try-catch" to make the process of editing and updating a Student object in the database more secure.
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,EnrollmentDate")] Student student)
+        public ActionResult EditPost(int? id)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
-
-        // GET: Students/Delete/5
-        public ActionResult Delete(int? id)
-        {
+            //if the ID passed into this method is null, displays an error page
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+            //attempts to locate the Student object in the database with the matching ID, and saves the object a new variable
+            var studentToUpdate = db.Students.Find(id);
+            //attempts to update the Student in the database with the new values
+            if (TryUpdateModel(studentToUpdate, "", new string[] { "LastName", "FirstName", "EnrollmentDate" }))
+            {
+                //attempts to save the changes made to the Student item in the database, and then display the "Student/Index" page
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                //handles any DataException errors that may occur
+                catch (DataException dex)
+                {
+                    //logs a standard error message
+                    ModelState.AddModelError("", "Unable to save changes. Try again later, and if the problem persists, contact your system administrator.");
+                    //logs the error message for the specific error that occurred in this instance
+                    ModelState.AddModelError("", dex.Message);
+                }
+            }
+
+            return View(studentToUpdate);
+        }
+
+        // GET: Students/Delete/5
+        // I  updated this Delete method to accomodate for the possibility of a faulty attempt at deleting an item in the database.
+        // In the event it encounters an error when trying to delete an object in the database, it will not go through with the attempy, and instead display a page with
+        // an error message and give the user the choice to either cancel or try again.
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
+        {
+            //if the ID passed into this method is null, displays an error page
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //if the "saveChangesError" variable passed into this method is true (which indicates an error has occurred), then display an error message saying the attempt
+            // to delete the object has failed
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists, contact your system administrator.";
+            }
+            //attempts to locate the Student object in the database with the matching ID
             Student student = db.Students.Find(id);
+            //if the student is not found, display am error page saying so
             if (student == null)
             {
                 return HttpNotFound();
@@ -106,13 +157,28 @@ namespace ContosoUniversity.Controllers
         }
 
         // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
+        // This used to be named "DeleteConfirmed", but was changed to just "Delete". Performs the actual delete operation. This method is technically called in conjuction
+        // with the other "Delete" method above this one
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            //attempts to delete the Student object
+            try
+            {
+                //locates the Student object in the database with the matching ID
+                Student student = db.Students.Find(id);
+                //Removes the specified student from the Students table in the database
+                db.Students.Remove(student);
+                //saves the changes made to the database
+                db.SaveChanges();
+            }
+            //handles any DataException errors that may occur
+            catch (DataException)
+            {
+                //displays the Delete page with the appropriate error
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
