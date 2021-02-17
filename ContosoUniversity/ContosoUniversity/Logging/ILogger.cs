618a453c901624ffb4df18026c6ad70133cdf25f
levit123 <30578168+levit123@users.noreply.github.com>
2021-02-17 13:01:27 -0800

    Logging interface and class
    -created an interface named "ILogger"
    -created a class named "Logger", which inherits from "ILogger"
    -this logs activity on the web app, and formats and returns an error message if an exception occurs
    
    Next thing to do is "Create Interceptor Classes"

---
+++ ContosoUniversity/ContosoUniversity/Logging/ILogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Logging
+{
+    //an interface for logging. The "Logger" class will inherit from this interface
+    public interface ILogger
+    {
+        void Information(string message);
+        void Information(string fmt, params object[] vars);
+        void Information(Exception exception, string fmt, params object[] vars);
+
+        void Warning(string message);
+        void Warning(string fmt, params object[] vars);
+        void Warning(Exception exception, string fmt, params object[] vars);
+
+        void TraceApi(string componentName, string method, TimeSpan timespan);
+        void TraceApi(string componentName, string method, TimeSpan timespan, string properties);
+        void TraceApi(string componentName, string method, TimeSpan timespan, string fmt, params object[] vars);
+    }
+}
